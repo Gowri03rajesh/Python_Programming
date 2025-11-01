ca66b78e77453d52aae20793610f8ff4df24c5db
Gowri03rajesh <gowri8442@gmail.com>
2025-11-01 22:26:47 +0530

    Add files via upload

---
+++ ntrytic_rate.py
@@ -0,0 +1,24 @@
+
+#My own code
+
+# Age=int(input("enter the age:"))
+# if Age<10:
+#     print("Rate is 7")
+# elif Age>=10 & Age<60:
+#     print("Rate is 10")
+# else:
+#     print("Rate is 5")
+#
+
+
+
+
+#Chatgpt code
+Age=int(input("enter the age:"))
+if Age<10:
+    Rate=7
+elif Age>=10 & Age<60:
+    Rate=10
+else:
+    Rate=5
+print("The entry ticket rate is:",Rate)
